587371897c2b4f5163266dddd95bef0e8f560b4a
Nemanja Stankovic <nemanjastan005@gmail.com>
2022-08-02 20:27:50 +0200

    Responsive design for landing page - DONE

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 document.querySelector('div.form label span').addEventListener('click', (e) => {
   e.preventDefault();
   document.querySelector('div.modal').style.display = 'block';
-  document.querySelector('div.modal').style.animation = 'float .55s';
+  document.querySelector('div.modal').style.animation = 'float .8s';
   document.querySelector('div.gradient').style.zIndex = '2000';
   document.querySelector('div.gradient').style.background = 'rgba(0,0,0,.9)';
 });
 
 document.querySelector('i').addEventListener('click', (e) => {
   e.preventDefault();
-  document.querySelector('div.modal').style.animation = 'close-float .6s';
+  document.querySelector('div.modal').style.animation = 'close-float .8s';
   setTimeout(function(){
     document.querySelector('div.modal').style.display = 'none';
-  }, 500);
+  }, 700);
   document.querySelector('div.gradient').style.zIndex = '100';
   document.querySelector('div.gradient').style.background = 'linear-gradient(to bottom, rgba(0,0,0,.7), rgba(0,0,0,.8))';
 });
